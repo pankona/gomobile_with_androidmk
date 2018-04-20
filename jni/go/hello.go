00649f173594c33edde194d583d2883c5ae643f6
pankona <yosuke.akatsuka@gmail.com>
2018-04-20 15:32:54 +0900

    add LDFLAGS -llog

--- jni/go/hello.go
+++ jni/go/hello.go
@@ -6,6 +6,7 @@ package hello
 #cgo arm   LDFLAGS: ../../obj/local/armeabi-v7a/libhello.a
 #cgo 386   LDFLAGS: ../../obj/local/x86/libhello.a
 #cgo amd64 LDFLAGS: ../../obj/local/x86_64/libhello.a
+#cgo LDFLAGS: -llog
 // gomobile bind doesn't support mips/mips64
 // #cgo mips LDFLAGS: ../../obj/local/mips/libhello.a
 // #cgo mips64 LDFLAGS: ../../obj/local/mips64/libhello.a
